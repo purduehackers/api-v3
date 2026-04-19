988f6269e4004d7516e48a1e37d372ed7220dc2f
Kian Kasad <kian@kasad.com>
2026-04-19 18:29:17 -0400

    feat: add Sentry integration

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import * as Sentry from "@sentry/cloudflare";
 import app from "./server";
 
 import Discord from "./actors/discord/index";
@@ -7,6 +8,17 @@ import Sign from "./actors/sign/index";
 
 export { Discord, Doorbell, Phonebell, Sign };
 
-export default {
-  fetch: app.fetch,
-} satisfies ExportedHandler<Env>;
+export default Sentry.withSentry(
+  (env) => ({
+    dsn: "https://d91cbfdeb0b0284d6ad54473db3e8fd4@o4510744753405952.ingest.us.sentry.io/4511249050959872",
+
+    // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+    tracesSampleRate: 1,
+
+    // Enable logs to be sent to Sentry
+    enableLogs: true,
+  }),
+  {
+    fetch: app.fetch,
+  } satisfies ExportedHandler<Env>,
+);
